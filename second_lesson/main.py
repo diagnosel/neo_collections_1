80aa1c977027bb0713bc998de0e865f0d4e30c07
AurikaS <aurikas@oddity.com>
2025-11-17 16:51:58 +0200

    if construction

---
+++ second_lesson/main.py
@@ -0,0 +1,47 @@
+#1
+# num = 15  # приклад значення для num
+
+# if num > 10:
+#     print("num більше за 10")
+# else:
+#     print("num не більше за 10")
+
+#2
+# x = int(input('Введіть число: '))
+
+# if x % 2 == 0:
+#     print("Число x є парним.")
+# else:
+#     print("Число x є непарним.")
+
+#3
+# a = input('Введіть число: ')
+# a = int(a)
+# if a > 0:
+#     print('Число додатне')
+# elif a < 0:
+#     print("Число від'ємне")
+# else:
+#     print('Це число - нуль')
+
+#4
+# money = 0
+# if money:
+#     print(f"You have {money} on your bank account")
+# else:
+#     print("You have no money and no debts")
+
+#5
+# result = None
+# if result:
+#     print(result)
+# else:
+#     print("Result is None, do something")
+
+#6
+# user_name = input("Enter your name: ")
+
+# if user_name:
+#     print(f"Hello {user_name}")
+# else:
+#     print("Hi Anonym!")
