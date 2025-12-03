7e2f6d0a25873e92c2cc1de6e0ad0391219e1d24
AurikaS <aurikas@oddity.com>
2025-12-04 01:04:39 +0200

    longest word

---
+++ third_lesson/longest_word.py
@@ -0,0 +1,10 @@
+def longest_word(string_of_words):
+    words = string_of_words.split()
+    max_word = words[0]
+    for word in words:
+        if len(word) > len(max_word):
+            max_word=word
+            
+    print(max_word)
+
+longest_word("hello and goodbuy")
