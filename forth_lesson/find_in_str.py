a9e109c75122ee46deda48f0f06d8b6505317cd2
AurikaS <aurikas@oddity.com>
2025-12-08 00:23:15 +0200

    Метод replace() також застосовують для видалення підрядка

--- forth_lesson/find_in_str.py
+++ forth_lesson/find_in_str.py
@@ -50,3 +50,8 @@ text = "one fish, two fish, red fish, blue fish"
 new_text = text.replace("fish", "bird", 2)
 print(new_text)  
 
+#text = "one fish, two fish, red fish, blue fish"
+text = "Hello, world!"
+new_text = text.replace(" world", "")
+print(new_text)
+
